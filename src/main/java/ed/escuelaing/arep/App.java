84875066473b382e976391e686cf80b5d3debb00
mariahv9 <mafehv1999@hotmail.com>
2020-08-14 17:16:43 -0500

    Actualizacion app

--- src/main/java/ed/escuelaing/arep/App.java
+++ src/main/java/ed/escuelaing/arep/App.java
@@ -1,13 +1,21 @@
 package ed.escuelaing.arep;
+import static spark.Spark.*;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        port (getPort());
+        get("/hello", (req, res) -> "Hello Heroku!!");
+    }
+
+    public static int getPort() {
+        if (System.getenv("PORT") != null)
+        {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
